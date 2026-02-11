d180f6676c92dee2ace138f5c233bc28313ac855
Ardika Bagus <me@ardikabs.com>
2026-02-11 23:54:08 +0700

    fix: this is fix for v1.0
    Signed-off-by: Ardika Bagus <me@ardikabs.com>

--- app/dummy.sh
+++ app/dummy.sh
@@ -3,3 +3,9 @@
 echo "This is a dummy script for testing purposes."
 echo "This is a supposed to be a fix issue#1"
 echo "This is a new feature as reported in issue#2"
+echo "This is a new feature as reported in issue#2"
+
+echo "testing"
+echo "feature"
+
+echo "fix for v1.0"
