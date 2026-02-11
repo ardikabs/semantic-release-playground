29799151d218dc4cbabd402be222ab89ec001f60
Ardika Bagus <me@ardikabs.com>
2026-02-12 02:34:10 +0700

    fix: helm chart values
    Signed-off-by: Ardika Bagus <me@ardikabs.com>

--- hack/sync-version.sh
+++ hack/sync-version.sh
@@ -31,10 +31,14 @@ if [ "$TYPE" == "app" ]; then
   fi
 elif [ "$TYPE" == "chart" ]; then
   echo "Updating chart version to $VERSION in $CHART_FILE..."
+  APP_VERSION=$(git describe --tags --abbrev=0 --match "v*")
+
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/^version: .*/version: $VERSION/" "$CHART_FILE"
+    sed -i '' "s/^version: .*/version: ${VERSION}/" "$CHART_FILE"
+    sed -i '' "s/^appVersion: .*/appVersion: ${APP_VERSION}/" "$CHART_FILE"
   else
-    sed -i "s/^version: .*/version: $VERSION/" "$CHART_FILE"
+    sed -i "s/^version: .*/version: ${VERSION}/" "$CHART_FILE"
+    sed -i "s/^appVersion: .*/appVersion: ${APP_VERSION}/" "$CHART_FILE"
   fi
 else
   echo "Error: Unknown type '$TYPE'. Use 'app' or 'chart'."
