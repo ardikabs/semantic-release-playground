e0467e13c63dc15c859abd7e0a0806b5315af08c
Ardika Bagus <me@ardikabs.com>
2026-02-16 21:24:37 +0700

    fix: testing
    Signed-off-by: Ardika Bagus <me@ardikabs.com>

--- app/feature-v1.3.sh
+++ app/feature-v1.3.sh
@@ -3,3 +3,5 @@
 echo "A new feature for version 1.3"
 echo "After thorough testing, this feature has a bug, this is the fix"
 echo "While we are not done an official release, the requirement changed, so we this is the feature to satisfy the new requirement"
+
+echo "testing sek"
